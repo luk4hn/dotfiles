7ec7d728da586a593491536921cef8d0bd9e2072
Huy Nguyen <xngqhy@gmail.com>
2015-08-29 12:53:00 +0700

    add global git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 _project_path=$(dirname $(readlink -f $0))
 
 ##
-for _file in .bashrc .screenrc .tmux.conf .Xdefaults ; do 
+for _file in .bashrc .screenrc .tmux.conf .Xdefaults .gitconfig ; do 
     ln -svf "$_project_path/$_file" "$HOME/$_file"
 done
