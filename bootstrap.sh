a648e6b53e0cf8049ad2a81d757c33b7503d3583
Huy Nguyen <luk4hn@gmail.com>
2016-09-05 21:33:39 +0700

    add compton.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,7 @@ _project_path=$(dirname $(readlink -f $0))
 for _file in .bashrc .screenrc .tmux.conf .Xdefaults .gitconfig .Xresources ; do 
     ln -svf "$_project_path/$_file" "$HOME/$_file"
 done
+
+for _file in i3 compton.conf ; do 
+    ln -svf "$_project_path/$_file" "$HOME/.config/$_file"
+done
