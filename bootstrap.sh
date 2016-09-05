1804df734fcff59cae75df37d987bcc14274cb0d
Huy Nguyen <luk4hn@gmail.com>
2016-09-05 21:38:35 +0700

    add zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 _project_path=$(dirname $(readlink -f $0))
 
 ##
-for _file in .bashrc .screenrc .tmux.conf .Xdefaults .gitconfig .Xresources ; do 
+for _file in .bashrc .zshrc .screenrc .tmux.conf .Xdefaults .gitconfig .Xresources ; do 
     ln -svf "$_project_path/$_file" "$HOME/$_file"
 done
 
